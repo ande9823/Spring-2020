0cefa47e60011ddbf65ac7f5f1ab726021f284c7
Anders Gaarsdal <angaa19@student.sdu.dk>
2020-02-15 19:02:23 +0100

    Added store

---
+++ Store/Store/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //ask a question
+            Console.WriteLine("what's your name?");
+            //read answer
+            string input = Console.ReadLine();
+            //repeat answer
+            Console.WriteLine(input + ", really?");
+            //Console.WriteLine("{0}, really?", input);
+
+            //wait for confirmation to quit
+            Console.Write("press enter to exit!");
+            Console.ReadLine();
+            */
+
+            Store st = new Store();
+            Console.WriteLine("Welcome to out Store! :)");
+            st.showcase.Show();
+
+            while(true)
+            {
+                // buy a game, see what games are there, quit the store, se descriptions
+                Console.WriteLine("Please enter a command (quit/show/buy)");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "quit":
+                        Environment.Exit(0);
+                        break;
+                    
+                    case "show":
+                        st.showcase.Show();
+                        break;
+
+                    case "buy":
+                        // ask what game
+                        Console.WriteLine("What game would you like to buy? Please write the number of the game!");
+                        string selectedGame = Console.ReadLine();
+                        int gameIndex = Convert.ToInt32(selectedGame);
+                        string gameName = null;
+                        if (gameIndex < st.showcase.games.Count) { }
+                            gameName = st.showcase.games[gameIndex].name;
+                        // remove it from showcase
+                        if (st.showcase.RemoveGame(gameIndex))
+                        {
+                            Console.WriteLine(gameName + " was successfully bought");
+                        }
+                        else
+                        {
+                            Console.WriteLine("something went wrong!");
+                        }
+                        // or maybe tell the user that that's not a valid game
+                        break;
+                    
+                    default:
+                        Console.WriteLine("I don't understand :(");
+                        break;
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Store/Store/Store.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    class Store
+    {
+        public Showcase showcase;
+
+        public Store()
+        {
+            showcase = new Showcase(5);
+
+            //we need to add games to the showcase
+            Game g1 = new Game("Braid", "timeywimey puzzle game", 10);
+            Game g2 = new Game("Warcraft 3", "bad cash grab", 40);
+            Game g3 = new Game("Doom", "demon bad, pewpew", 1);
+
+            //we need to add games to the showcase
+            showcase.AddGame(g1);
+            showcase.AddGame(g2);
+            showcase.AddGame(g3);
+
+            DLC d1 = new DLC("value pack", "more cash grab", 10000);
+            g2.dlcs.Add(d1);
+        }
+
+        //buying
+        //seeing whats for sale
+    }
+}
